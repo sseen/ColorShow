abefe2777fe0c816cf1a75479676de2d41795bc2
sseen <ssnfee@gmail.com>
2021-04-03 20:06:32 +0800

    Initial Commit

---
+++ ColorShow/ColorShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorShowApp.swift
+//  ColorShow
+//
+//  Created by solo on 2021/4/3.
+//
+
+import SwiftUI
+
+@main
+struct ColorShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorShow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorShow
+//
+//  Created by solo on 2021/4/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
